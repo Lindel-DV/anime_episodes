4096c3405c3ea42ca466832bebd8ad3b2a6af905
LD Vitali <100724040+Lindel-DV@users.noreply.github.com>
2025-12-17 18:05:16 -0500

    Update shared_lists.py

--- shared_lists.py
+++ shared_lists.py
@@ -1,27 +1,41 @@
-# You will create your lists with the following information:
-# Search_list = ["Title", "Author", "Artist", "Main_Character1", "Main_Character", ""Main_Character3",  "Year_Written", "My_Personal_opnion"]
-# You will have to add all the information, as well research how to make it so add many lines to your list.
+# Instructions for creating lists:
+# Here is the structure you will use for the 'Search_list':
+# Include key pieces of information: ["Title", "Author", "Artist", "Main_Character1", "Main_Character2", "Main_Character3", "Year_Written", "My_Personal_Opinion"]
 
+# Note that you should provide real values for each field when using 'Search_list'.
+# Example:
+# Search_list = ["Naruto", "Masashi Kishimoto", "Masashi Kishimoto", "Naruto Uzumaki", "Sasuke Uchiha", "Sakura Haruno", 1999, "A nostalgic masterpiece!"]
 
-#    # Example: a list of valid user roles
-#    USER_ROLES = ["admin", "moderator", "viewer"]
+# To create and add multiple entries, you can use methods such as 'append' or a more complex data structure like a list of dictionaries.
 
-#    # Example: a list of supported countries
-#    SUPPORTED_COUNTRIES = ["US", "JM", "CA", "UK"]
+# Example of handling multiple entries:
+Anime_Episodes = [
+    {
+        "Title": "Naruto",
+        "Author": "Masashi Kishimoto",
+        "Artist": "Masashi Kishimoto",
+        "Main_Character1": "Naruto Uzumaki",
+        "Main_Character2": "Sasuke Uchiha",
+        "Main_Character3": "Sakura Haruno",
+        "Year_Written": 1999,
+        "My_Personal_Opinion": "A nostalgic masterpiece!"
+    },
+    {
+        "Title": "Attack on Titan",
+        "Author": "Hajime Isayama",
+        "Artist": "Hajime Isayama",
+        "Main_Character1": "Eren Yeager",
+        "Main_Character2": "Mikasa Ackerman",
+        "Main_Character3": "Armin Arlert",
+        "Year_Written": 2009,
+        "My_Personal_Opinion": "A thrilling and dark adventure."
+    }
+]
 
-# program1.py
-from shared_lists import USER_ROLES, SUPPORTED_COUNTRIES
+# Example Usage:
+from shared_lists import Anime_Episodes, USER_ROLES, SUPPORTED_COUNTRIES
 
 print("Allowed roles:", USER_ROLES)
 if "admin" in USER_ROLES:
     print("Admin access granted.")
 
-#    # You can also use more complex data
-#    Anime_Episodes = [
-#        {"Title", "Author", "Artist", "Main_Character1", "Main_Character", ""Main_Character3",  "Year_Written", "My_Personal_opnion"},
-#        {"Title", "Author", "Artist", "Main_Character1", "Main_Character", ""Main_Character3",  "Year_Written", "My_Personal_opnion"},
-#        {"Title", "Author", "Artist", "Main_Character1", "Main_Character", ""Main_Character3",  "Year_Written", "My_Personal_opnion"}
-#    ]
-
-
-search_list = 
