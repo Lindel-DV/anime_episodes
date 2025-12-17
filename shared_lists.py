dc19af0281f6d497f4ea39a49f49efb0ca258565
LD Vitali <100724040+Lindel-DV@users.noreply.github.com>
2025-12-17 15:10:15 -0500

    Fix syntax and formatting in shared_lists.py

---
+++ shared_lists.py
@@ -0,0 +1,27 @@
+# You will create your lists with the following information:
+# Search_list = ["Title", "Author", "Artist", "Main_Character1", "Main_Character", ""Main_Character3",  "Year_Written", "My_Personal_opnion"]
+# You will have to add all the information, as well research how to make it so add many lines to your list.
+
+
+#    # Example: a list of valid user roles
+#    USER_ROLES = ["admin", "moderator", "viewer"]
+
+#    # Example: a list of supported countries
+#    SUPPORTED_COUNTRIES = ["US", "JM", "CA", "UK"]
+
+# program1.py
+from shared_lists import USER_ROLES, SUPPORTED_COUNTRIES
+
+print("Allowed roles:", USER_ROLES)
+if "admin" in USER_ROLES:
+    print("Admin access granted.")
+
+#    # You can also use more complex data
+#    Anime_Episodes = [
+#        {"Title", "Author", "Artist", "Main_Character1", "Main_Character", ""Main_Character3",  "Year_Written", "My_Personal_opnion"},
+#        {"Title", "Author", "Artist", "Main_Character1", "Main_Character", ""Main_Character3",  "Year_Written", "My_Personal_opnion"},
+#        {"Title", "Author", "Artist", "Main_Character1", "Main_Character", ""Main_Character3",  "Year_Written", "My_Personal_opnion"}
+#    ]
+
+
+search_list = 
